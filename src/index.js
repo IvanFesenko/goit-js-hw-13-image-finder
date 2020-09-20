0e158e9fb17df4da218d770346ae52eb8e762a0e
Ivan Fesenko <pharmivan@gmail.com>
2020-09-20 17:24:04 +0300

    lang select add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'material-design-icons/iconfont/material-icons.css';
 import './scss/basicLightBox.scss';
+import './js/Select/styles.scss';
 
 import './css/styles.css';
 import './scss/base.scss';
--- src/js/scrypt.js
+++ src/js/scrypt.js
@@ -1,5 +1,6 @@
 import error from './notify';
 import galleryTemplate from '../templates/gallery.hbs';
+import { Select } from './select/select';
 
 const basicLightbox = require('basiclightbox');
 
@@ -11,18 +12,32 @@ const refs = {
   form: document.querySelector('.search-form'),
 };
 
+new Select('#select', {
+  selectedId: '1',
+  data: [
+    { id: '1', value: 'en' },
+    { id: '2', value: 'ru' },
+    { id: '3', value: 'de' },
+    { id: '4', value: 'fr' },
+    { id: '5', value: 'it' },
+  ],
+  onSelect(item) {
+    ImageFinder.lenguage = item.value;
+  },
+});
+
 const ImageFinder = {
   key: 'key=18267918-a545f4b922b3d8b59313b99e1',
   page: 1,
+  lenguage: 'en',
 };
 
 refs.btnSearch.addEventListener('click', onClickSearch);
-// refs.btnLoadMore.addEventListener('click', onClickLoadMore);
 refs.gallery.addEventListener('click', onImageClick);
 window.addEventListener('scroll', loadMore);
 
 function getURL() {
-  return `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${ImageFinder.searchQuery}&page=${ImageFinder.page}&per_page=12&${ImageFinder.key}`;
+  return `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${ImageFinder.searchQuery}&page=${ImageFinder.page}&lang=${ImageFinder.lenguage}&per_page=12&${ImageFinder.key}`;
 }
 
 async function getData(url) {
